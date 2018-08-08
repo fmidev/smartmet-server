c439ecee36cee3f6c55e59d495e0a816a88b341e
Mika Heiskanen <mika.heiskanen@fmi.fi>
2018-08-08 13:17:58 +0300

    Use make_shared instead of new

--- source/AsyncServer.cpp
+++ source/AsyncServer.cpp
@@ -97,16 +97,16 @@ void AsyncServer::startAccept()
     // Make a new connection object and let it wait for an incoming connection
     // This should not need locking, since we accept connections from a single socket (handleAccepts
     // are implicity serialized)
-    itsNewConnection.reset(new AsyncConnection(this,
-                                               itsCanGzip,
-                                               itsCompressLimit,
-                                               itsMaxRequestSize,
-                                               itsTimeout,
-                                               itsDumpRequests,
-                                               itsIoService,
-                                               itsReactor,
-                                               itsSlowExecutor,
-                                               itsFastExecutor));
+    itsNewConnection = boost::make_shared<AsyncConnection>(this,
+                                                           itsCanGzip,
+                                                           itsCompressLimit,
+                                                           itsMaxRequestSize,
+                                                           itsTimeout,
+                                                           itsDumpRequests,
+                                                           itsIoService,
+                                                           itsReactor,
+                                                           itsSlowExecutor,
+                                                           itsFastExecutor);
     itsAcceptor.async_accept(
         itsNewConnection->socket(),
         boost::bind(&AsyncServer::handleAccept, this, boost::asio::placeholders::error));
