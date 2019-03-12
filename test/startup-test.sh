9442d100d83d91c65d0c0ff732d047345b2bb6a3
Heikki Pernu <heikki.pernu@nekonet.fi>
2019-03-12 11:58:41 +0200

    Fix server exit detection

--- test/startup-test.sh
+++ test/startup-test.sh
@@ -14,10 +14,16 @@ fi
 function waitloop {
     echo "Waiting for server to start ..."
     while ! grep -q "Launched.*server" test.out ; do
-	# Server not fully started yet
-	sleep 1s
-	# If server stopped(timeout or crash), return
-	if [ -e "test.status" ] ; then return `cat test.status` ; fi
+	   # Server not fully started yet
+    	sleep 1s
+    	# If server stopped(timeout or crash), return
+    	if [ ! -d /proc/$childpid ]; then
+    	   wait
+    	   ret=$?
+    	   if [ "$ret" != "0" ] ; then return $ret ; fi
+    	   echo "Failure to detect subprocess existence"
+    	   return 1
+    	fi
     done
     echo "Server started, shutting it down ..."
     # Detected started string, wait a bit
@@ -29,29 +35,26 @@ function waitloop {
     return $? # Normal return,  
 }
 
-rm -f test.status  # Appearance of this file signals smartmetd exited
 touch test.out # Avoid missing file warnings when grepping
-( timeout $waittime "$smartmetd" -d -v -c ./minimal.conf >test.out 2>&1 ; echo $? > test.status ) &
+timeout $waittime "$smartmetd" -d -v -c ./minimal.conf >test.out 2>&1 &
 childpid=$!
 
 waitloop
 ret=$?
-tmret=0
-if [ -r test.status ] ; then
-    tmret=`cat test.status`
-fi
 
 if [ "$ret" != "0" ] ; then echo "Abnormal end." ; fi
 
 # The rest should fail if any of the steps fail
 set -e
-echo ; echo "Server exited with status $tmret, output:"
+echo ; echo "Server exited with status $ret, output:"
 cat test.out
+echo
+echo "Checking for output correctness"
+grep "Launched.*server" test.out
+grep "SmartMet.*stopping" test.out
+
 rm -f test.out
-
-# Likely timeout or crash
-if [ "$tmret" != "0" ] ; then exit $tmret ; fi
-
+echo
 # Normal ecit/shutdown
 exit $ret
 
